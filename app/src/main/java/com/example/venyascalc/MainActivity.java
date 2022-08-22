da80c34230b7ad514f8afd99274b7dc452d79f73
sharma1208 <90787231+sharma1208@users.noreply.github.com>
2022-08-22 12:54:23 -0500

    made an addition,subtraction, and multiplication button
    need to finish the multiplication code in MainActivity.java and want to see if after typing in an integer, we can change the screen to ask for the operation wanted.

--- app/src/main/java/com/example/venyascalc/MainActivity.java
+++ app/src/main/java/com/example/venyascalc/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.venyascalc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void findSum(View view){
+        EditText firstNum = findViewById(R.id.firstNum);
+        EditText secondNum = findViewById(R.id.secondNum);
+        TextView finalSum = findViewById(R.id.resultant);
+
+        int parse1 = Integer.parseInt((firstNum.getText().toString()));
+        int parse2 = Integer.parseInt((secondNum.getText().toString()));
+
+        int sum = parse1 + parse2;
+
+        finalSum.setText(" " + sum);
+
+    }
+
+    public void findSubtraction(View view){
+        EditText firstNum = findViewById(R.id.firstNum);
+        EditText secondNum = findViewById(R.id.secondNum);
+        TextView finalSum = findViewById(R.id.resultant);
+
+        int parse1 = Integer.parseInt((firstNum.getText().toString()));
+        int parse2 = Integer.parseInt((secondNum.getText().toString()));
+
+        int sum = parse1 - parse2;
+
+        finalSum.setText(" " + sum);
+
+    }
+
+
+
 }
